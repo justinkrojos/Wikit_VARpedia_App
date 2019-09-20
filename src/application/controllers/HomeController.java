0eb8d34cc71656666c29ae88529a7948b2ed06c7
Tianrens <tshe695@aucklanduni.ac.nz>
2019-09-20 14:17:26 +1200

    Created Home Stage

--- src/application/controllers/HomeController.java
+++ src/application/controllers/HomeController.java
@@ -1,8 +1,32 @@
 package application.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class HomeController {
-    private Text text;
+
+    @FXML
+    private Text _title;
+
+    @FXML
+    private Text _creationTitle;
+
+    @FXML
+    private ListView _creationList;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnDel;
+
+    @FXML
+    private Button btnCreate;
+
+    @FXML
+    private Pane _player;
 
 }
