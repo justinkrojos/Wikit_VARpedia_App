88129c17a76ab47526599a245f9f0524ac67d043
Tianrens <tshe695@aucklanduni.ac.nz>
2019-09-21 16:06:10 +1200

    Added getCreationsDir

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,11 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main extends Application {
 
     private Stage _primaryStage;
+
+    //private String _creationDir;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -30,7 +34,44 @@ public class Main extends Application {
         return _primaryStage;
     }
 
+    public static String getCreationDir() {
+        String creationsDir = null;
+        try {
+            creationsDir = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getAbsolutePath();
+            creationsDir = creationsDir.substring(0,creationsDir.lastIndexOf("/"));
+/*            System.out.println(creationsDir);
+            String systemDir = System.getProperty("user.dir");
+            System.out.println(systemDir);*/
+            creationsDir = creationsDir + "/creations";
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return creationsDir;
+    }
+
     public static void main(String[] args) {
-        launch(args);
+        try {
+/*            String systemDir = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getAbsolutePath();
+            systemDir = systemDir.substring(0,systemDir.lastIndexOf("/"));
+            //System.out.println(systemDir);
+
+            //String systemDir = System.getProperty("user.dir");
+            String creationsDir = systemDir + "/creations";*/
+
+            String creationsDir = getCreationDir();
+            String cmd = "[ ! -d " + creationsDir + " ]";
+            ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
+
+            Process process = pb.start();
+            //System.out.println(process.waitFor());
+            if (process.waitFor() == 0) {
+                cmd = "mkdir -p " + creationsDir;
+                ProcessBuilder mkdirpb = new ProcessBuilder("bash", "-c", cmd);
+                Process mkdirP = mkdirpb.start();
+            }
+            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
