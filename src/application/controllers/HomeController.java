cbb8158ee084e1d64ab4bc232ea7995d64b368d0
Tianrens <tshe695@aucklanduni.ac.nz>
2019-09-21 22:27:02 +1200

    Added Delete button functionality

--- src/application/controllers/HomeController.java
+++ src/application/controllers/HomeController.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -17,11 +19,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Optional;
 
 public class HomeController {
 
     private final File _folder = new File(Main.getCreationDir());
     private ObservableList<String> _items;
+    private String _selectedItem = null;
 
     @FXML
     private Text _title;
@@ -50,13 +54,40 @@ public class HomeController {
     @FXML
     private void handleBtnPlay() {
         System.out.println("Playing");
-       // System.out.println(Main.getCreationDir());
+        if (_selectedItem == null) {
+            alertNullSelection();
+            return;
+        }
 
     }
 
     @FXML
     private void handleBtnDel() {
         System.out.println("Deleting");
+        if (_selectedItem == null) {
+            alertNullSelection();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Deleting...");
+        alert.setHeaderText("Are you sure you want to delete " + _selectedItem + "?");
+        alert.setContentText("Are you sure? Press okay to delete, cancel to keep.");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            String delCmd = "rm -r "+ Main.getCreationDir() + "/" + _selectedItem;
+            ProcessBuilder delBuilder = new ProcessBuilder("bash","-c",delCmd);
+
+            Process delProcess = null;
+            try {
+                delProcess = delBuilder.start();
+                delProcess.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            updateListTree();
+        }
     }
 
     @FXML
@@ -92,12 +123,28 @@ public class HomeController {
      */
     private ArrayList<String> listFilesOfFolder(final File folder) {
         ArrayList<String> list = new ArrayList<String>();
+
         for (final File fileEntry : folder.listFiles()) {
-            list.add(fileEntry.getName().replace(".mp4", ""));
+            list.add(fileEntry.getName());//.replace(".mp4", ""));
         }
 
         Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
         return list;
+    }
+
+    @FXML
+    private void selectItem() {
+        _selectedItem = (String) _creationList.getSelectionModel().getSelectedItem();
+        System.out.println(_selectedItem);
+
+    }
+
+    private void alertNullSelection() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Please Select A Creation");
+        alert.setHeaderText("No item has been selected.");
+        alert.setContentText("Pease select a creation");
+        alert.showAndWait();
     }
 
 }
