c94ebb2864538808e81446484dc244e52dd262fd
Tianrens <tshe695@aucklanduni.ac.nz>
2019-09-21 23:30:57 +1200

    CreateController stuff

--- src/application/controllers/CreateController.java
+++ src/application/controllers/CreateController.java
@@ -1,7 +1,31 @@
 package application.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateController {
 
+    @FXML
+    private TextArea _textArea;
+
+    @FXML
+    private TextField _termField;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private TextField _creationNameField;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnCreate;
 }
