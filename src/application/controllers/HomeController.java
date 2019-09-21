7522b1961a597960fd9c9142028a126211c99a6d
Tianrens <tshe695@aucklanduni.ac.nz>
2019-09-21 21:43:04 +1200

    Viewing creations added

--- src/application/controllers/HomeController.java
+++ src/application/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package application.controllers;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,9 +13,15 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class HomeController {
+
+    private final File _folder = new File(Main.getCreationDir());
+    private ObservableList<String> _items;
 
     @FXML
     private Text _title;
@@ -66,6 +74,34 @@ public class HomeController {
     @FXML
     public void handleBtnRefresh() {
         System.out.println("Refresh");
+        refreshList();
+    }
+
+    public void refreshList() {
+        updateListTree();
+    }
+
+    /**
+     * Get the creations in the folder.
+     */
+    public void updateListTree() {
+        _items = FXCollections.observableArrayList(listFilesOfFolder(_folder));
+        _creationList.setItems(_items);
+    }
+
+    /**
+     * Get the creations in the folder into an arrylist sorted.
+     * @param folder
+     * @return
+     */
+    private ArrayList<String> listFilesOfFolder(final File folder) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (final File fileEntry : folder.listFiles()) {
+            list.add(fileEntry.getName().replace(".mp4", ""));
+        }
+
+        Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+        return list;
     }
 
 }
